507d8387bed2116df519f15b7cbaaaba952e8d5f
Shadow-Slayer <wander.p.c.07@gmail.com>
2016-07-16 02:03:36 -0300

    Update bot.js
    alterado link bot simsimi, no botson

--- bot.js
+++ bot.js
@@ -3964,7 +3964,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                            $.getScript('https://dl.dropboxusercontent.com/s/b65say96wmcjnjn/bots.js');
+                            $.getScript('https://rawgit.com/Shadow-Slayer/mib/master/simsimi.js');
                             API.chatLog("!cmddel");
                     }
                 }
