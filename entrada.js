aa8cd6c8bbf5ffcbe48696e010f3c726eaa1f152
Shadow-Slayer <wander.p.c.07@gmail.com>
2016-07-21 16:25:41 -0300

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -11,6 +11,10 @@ if (user.username == "Awayshift") {
 API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " acabou de entrar!.");
 }
 
+if (user.username == "1380997") {
+API.sendChat("/me Viadão entrou");
+}
+
 if (user.username == "VzachPlay") {
 API.sendChat("/me O melhor segurança  @" + user.username + " entrou na sala!.");
 }      
