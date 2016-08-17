a64b8fd8a58bfc06aa67186fe74e5e5c47daad49
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-08-17 18:48:11 -0300

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -1,22 +1,14 @@
 API.on(API.USER_JOIN, function(user) {
-if (user.id == "Shadow Slayer") {
-API.sendChat("/me Shadoso ;) ");
-}
-
 if (user.id == "4080795") {
 API.sendChat("/me Shaaadoso <3 ");
 }
 
-if (user.username == "Eduarda Correa") {
+ (user.username == "5401598") {
 API.sendChat("/me A Rainha dos Pirralhos @" + user.username + " entrou na sala :babby: ");
 }
 
-if (user.username == "Awayshift") {
+if (user.username == "1380997") {
 API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " acabou de entrar!.");
-}
-
-if (user.id == "1380997") {
-API.sendChat("/me Viadão entrou");
 }
 
 if (user.username == "VzachPlay") {
