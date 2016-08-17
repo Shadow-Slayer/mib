6e25fa02abf55a1dbcd7d59b73590ce24771136a
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-08-17 19:39:10 -0300

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -3,15 +3,15 @@ if (user.id == "4080795") {
 API.sendChat("/me Shaaadoso <3 ");
 }
 
-if (user.username == "5401598") {
+if (user.id == "5401598") {
 API.sendChat("/me Cuidem de seus corações,pois a conquistadora  @" + user.username + " acaba de entrar na sala :kappa:");
 }
 
-if (user.username == "1380997") {
+if (user.id == "1380997") {
 API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " acabou de entrar!.");
 }
 
-if (user.username == "VzachPlay") {
+if (user.id == "4371581") {
 API.sendChat("/me O melhor segurança  @" + user.username + " entrou na sala!.");
 }      
 });
