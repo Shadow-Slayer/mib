254d149cb85183f1553829c9fa60e6d2663128e6
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-10-31 18:33:25 -0200

    Update bot-halloween.js

--- bot-halloween.js
+++ bot-halloween.js
@@ -3331,7 +3331,7 @@
 	
             rouletteppCommand: {
                 command: 'roletapp',
-                rank: 'cohost',
+                rank: 'manager',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
