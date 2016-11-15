dac60eccee138fc9455449dcca021e9f157e41e7
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-11-15 21:59:41 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3742,6 +3742,42 @@
                 }
             },
 
+	     tretarCommand: {
+                command: 'tretar',
+                rank: 'user',
+                type: 'startsWith',
+                getTretar: function (chat) {
+                    var c = Math.floor(Math.random() * basicBot.chat.tretas.length);
+                    return basicBot.chat.tretas[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eattretar);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousertretar, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selftretar, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.tretar, {nameto: user.username, namefrom: chat.un, tretar: this.getTretar()}));
+                            }
+                        }
+                    }
+                }
+            },
+		
             unbanCommand: {
                 command: 'unban',
                 rank: 'bouncer',
