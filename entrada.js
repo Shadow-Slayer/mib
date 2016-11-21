cca14a891dedc0152e2bb6df1527d030f4cfc0f2
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-11-21 18:33:48 -0200

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -2,10 +2,11 @@ API.on(API.USER_JOIN, function(user) {
 /*if (user.id == "4080795") {
 API.sendChat("/me Shaaadoso <3 ");
 }*/
-
+  
+/*
 if (user.id == "5401598") {
 API.sendChat("/me   @" + user.username + " Musa do Plug e Rainha da Move It acaba de entrar na sala.:crown:");
-}
+} */
 
 if (user.id == "1380997") {
 API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " acabou de entrar!.");
